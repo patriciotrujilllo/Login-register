bcd35e41f49ade33f8040908fed5faa4a41412d6
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-10 19:38:04 -0400

    Se cambio register.js por form.js

--- frontend/view/register.php
+++ frontend/view/register.php
@@ -50,7 +50,7 @@
   </div>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js"></script>
   <script src="../controlador/userControler.js"></script>
-  <script src="./register.js"></script>
+  <script src="./form.js"></script>
 </body>
 
 </html>
