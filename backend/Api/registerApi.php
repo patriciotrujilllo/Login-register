52ddf7fde51135a47ff57ff85f2d9b69ea25a672
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-10 18:35:38 -0400

    Ahora la Api espera la respuesta ya sea true o false para enviar el mensaje correspondiente

--- backend/Api/registerApi.php
+++ backend/Api/registerApi.php
@@ -8,9 +8,14 @@ switch ($_SERVER['REQUEST_METHOD']) {
     case 'POST':
         $data = json_decode(file_get_contents('php://input'), true);
         $usuario = new Usuario($data['email'], $data['password']);
-        if ($usuario->userRegister($data['email'], $data['password'])) {
+        $request = $usuario->userRegister($data['email'], $data['password']);
+        if ($request) {
             http_response_code(201);
             echo json_encode(['mensaje' => 'El usuario se creo correctamente.']);
+            exit;
+        } else {
+            http_response_code(401);
+            echo json_encode(['error' => 'el usuario ya fue creado.']);
             exit;
         }
 
