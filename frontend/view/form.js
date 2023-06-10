9aab00822e6402a564fd8a27e0ed2a583dbb5180
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-10 19:37:19 -0400

    Se decidio crear este nuevo archivo para refactorizar y evitar codigo duplicado

---
+++ frontend/view/form.js
@@ -0,0 +1,20 @@
+const handleFormSubmit = (event, callback) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    callback(data);
+}
+
+formLogin = document.querySelector('#formLogin');
+formRegister = document.querySelector('#formRegister');
+
+if (formLogin) {
+    formLogin.addEventListener("submit", function (event) {
+        handleFormSubmit(event, login);
+    });
+}
+if (formRegister) {
+    formRegister.addEventListener("submit", function (event) {
+        handleFormSubmit(event, register);
+    });
+}
