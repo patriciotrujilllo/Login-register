b82df27d6b51c88a1e33392d1e72d198d083d0d1
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-06-11 14:02:05 -0400

    Para evitar que se ingrese con el link directo se agregan las condiciones, que existe la sesion la cookie y que sean iguales

--- frontend/view/Home.php
+++ frontend/view/Home.php
@@ -1,3 +1,16 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['token']))
+  header("location:./login.php");
+
+if (!isset($_COOKIE['token']))
+  header("location:./login.php");
+
+if ($_SESSION['token'] != $_COOKIE['token'])
+  header("location:./login.php");
+
+?>
 <!DOCTYPE html>
 <html>
 
@@ -7,7 +20,7 @@
 
 <body>
   <h1>Hola <?php echo $_COOKIE["email"] ?></h1>
-  <!--<span><a href="logout.php">Logout</a></span>-->
+  <span><a href="../logout.php">Logout</a></span>
 </body>
 
 </html>
